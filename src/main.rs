80de1269bab36671f6a470a7c0cca45016fc3491
Ganesha Danu <ganeshadanu1@gmail.com>
2019-02-08 03:03:47 +0700

    add visualization

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,34 @@ mod simulatedAnnealing;
 
 use simulatedAnnealing::Solver;
 
+extern crate gnuplot;
+
 fn main() {
-    let mut solver = Solver::new(100000000000000.0, 0.1); 
+
+    use gnuplot::{Figure, Caption, Color, PointSymbol, ContourStyle, AutoOption};
+	
+	let mut solver = Solver::new(100000000000000.0, 0.1); 
     let solution = solver.solve();
-    println!("{}", Solver::search_area(solution));
+	
+	let x = [solution[0]];
+	let y = [solution[1]];
+	let z = [Solver::search_area(solution)];
+
+	let mut mat: [f64; 400] = [0.0; 400];
+	for i in 0..20 {
+		for j in 0..20 {
+			mat[i * j] = Solver::search_area([i as f64 - 10.0, j as f64 - 10.0]);	
+		}
+	}
+
+	let mut figure = Figure::new();
+	figure
+		.axes3d()
+		.surface(mat.iter(), 20, 20, Some((-10.0, -10.0, 10.0, 10.0)), &[Caption("Surface")])
+		.show_contours(true, false, ContourStyle::Linear, AutoOption::Auto, AutoOption::Auto)
+		.points(&x, &y, &z, &[Color("blue"), PointSymbol('S')]);
+	figure.show();
+
+	println!("{}", Solver::search_area(solution));
     println!("{:?}", solution);
 }
